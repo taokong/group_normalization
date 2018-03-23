31ea5fbe83a86d4321190dbb27a79ae399452b33
taokong <kt14@mails.tsinghua.edu.cn>
2018-03-23 15:52:24 +0800

    init, under testing

---
+++ group_norm.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+class GroupBatchnorm2d(nn.Module):
+    def __init__(self, c_num, group_num = 16, eps = 1e-10):
+        super(GroupBatchnorm2d,self).__init__()
+        self.group_num = group_num
+        self.gamma = nn.Parameter(torch.ones(c_num, 1, 1))
+        self.beta = nn.Parameter(torch.zeros(c_num, 1, 1))
+        self.eps = eps
+
+    def forward(self, x):
+        N, C, H, W = x.size()
+
+        x = x.view(N, self.group_num, -1)
+
+        self.mean = x.mean(dim = 2, keepdim = True)
+        self.std = x.std(dim = 2, keepdim = True)
+
+        x = (x - self.mean) / (self.std+self.eps)
+        x = x.view(N, C, H, W)
+
+        return x * self.gamma + self.beta
